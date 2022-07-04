69590728c3001d79dbe15ba782722f2fcc88462d
Keketso Ramosedi <kekeramosedi@gmail.com>
2022-07-04 21:47:17 +0200

    Added new GameManager class and new UI for starting game, modified BallController logic

--- MobileGameTraining/Assets/Scripts/BallController.cs
+++ MobileGameTraining/Assets/Scripts/BallController.cs
@@ -6,6 +6,8 @@ public class BallController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float bounceForce;
+    bool gameStarted;
+    
 
     void Awake(){
         rb = GetComponent<Rigidbody2D>();
@@ -19,15 +21,33 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown){
-            StartBounce();
+        if (!gameStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                StartBounce();
+                gameStarted = true;
+                GameManager.instance.GameStart();
+            }
         }
+       
     }
     
     void StartBounce()
     {
        Vector2 randomDirection = new Vector2(Random.Range(-1, 1), 1);
        rb.AddForce(randomDirection * bounceForce, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "FallCheck")
+        {
+            GameManager.instance.Restart();
+        }else if(collision.gameObject.tag == "Paddle")
+        {
+            GameManager.instance.ScoreUp();
+        }
     }
 }
 
